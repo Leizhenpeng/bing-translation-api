137cce80b46a61ca3ca50ef1f283ffff60389929
river <laolei@forkway.cn>
2024-05-11 23:36:10 +0800

    Update next.config.mjs and tsconfig.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        esmExternals: false  // 强制所有导入的外部依赖使用 CommonJS，这可以帮助处理一些兼容性问题。
-    }
-};
+const nextConfig = {}
 
 export default nextConfig;
